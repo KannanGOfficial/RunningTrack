75a73b1d48ed71c7c156f630e2ecb13e6b10115a
KannanGOfficial <kannang143592@gmail.com>
2023-12-28 16:42:02 +0530

    Google map dependency and api key is added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
-
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -83,5 +83,9 @@ dependencies {
 
     //Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
+
+    // Maps SDK for Android
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,14 @@ plugins {
     id("com.google.devtools.ksp") version ("1.9.0-1.0.13") apply false
     id("com.google.dagger.hilt.android") version "2.47" apply false
 }
+
+buildscript {
+    val navVersion = "2.7.6"
+    val androidGradlePlugin = "8.1.1"
+
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+        classpath("com.android.tools.build:gradle:$androidGradlePlugin")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
