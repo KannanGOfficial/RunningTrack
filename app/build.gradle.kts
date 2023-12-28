877c0a6da68b2d6d338b7ce23ce7c33af79771c2
KannanGOfficial <kannang143592@gmail.com>
2023-12-28 11:46:16 +0530

    compile and target SDK version is set to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.kannan.runningtrack"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.kannan.runningtrack"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
