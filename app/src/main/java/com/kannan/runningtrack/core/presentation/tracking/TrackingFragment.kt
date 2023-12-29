185be51a4af9691e4e8c90ccfb49bbec88e9ea9e
KannanGOfficial <kannang143592@gmail.com>
2023-12-29 17:09:11 +0530

    map lifecycles are added in the Tracking Fragment

--- app/src/main/java/com/kannan/runningtrack/core/presentation/tracking/TrackingFragment.kt
+++ app/src/main/java/com/kannan/runningtrack/core/presentation/tracking/TrackingFragment.kt
@@ -3,14 +3,61 @@ package com.kannan.runningtrack.core.presentation.tracking
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
 import com.kannan.runningtrack.R
 import com.kannan.runningtrack.databinding.FragmentTrackingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+
+    private lateinit var binding: FragmentTrackingBinding
+    private var map : GoogleMap? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentTrackingBinding.bind(view)
+
+        binding = FragmentTrackingBinding.bind(view)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync{
+            map = it
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+     }
 }
